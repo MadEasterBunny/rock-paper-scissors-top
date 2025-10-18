b151e59d9814882a9c9bc73949838453ad6bb712
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2025-10-18 20:43:55 +0900

    Add getComputerChoice Functions

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+
+const getRandomNum = num =>{
+    return Math.floor(Math.random() * num);
+}
+
+const getComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    const choiceInx = getRandomNum(choices.length);
+    return choices[choiceInx];
+}
+
+console.log(getComputerChoice());
