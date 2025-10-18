953a5e00d76f43408308b465fe1b107f4f84da85
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2025-10-18 21:13:49 +0900

    Add playRound Function

--- script.js
+++ script.js
@@ -14,7 +14,35 @@ const getComputerChoice = () => {
 
 const getHumanChoice = () => {
     const choice = prompt("Enter either rock, paper or scissors", "");
-    return choice;
+    return choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+const playRound = (humanChoice, computerChoice) => {
+    if(
+        humanChoice === "rock" && computerChoice === "scissors"||
+        humanChoice === "paper" && computerChoice === "rock"||
+        humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("Player wins!");
+        humanScore ++;
+    } else if (
+        humanChoice === "rock" && computerChoice === "paper" ||
+        humanChoice === "paper" && computerChoice === "scissors" ||
+        humanChoice === "scissors" && computerChoice === "rock"
+    ) {
+        computerScore++;
+        if(computerChoice === "rock"){
+            console.log("You lose! Rock beats Scissors");
+        } else if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock");
+        } else {
+            console.log("You lose! Scissors beats Paper");
+        }
+    } else {
+        console.log("It's a tie!")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection), humanSelection, computerSelection, humanScore, computerScore);
